a425b2029a0901961a33009930aae3224bef53cf
tony <tony@172.16.0.90>
2017-05-04 17:45:55 +0800

    增加七牛图片样式配置项。

--- src/main/java/org/b3log/solo/model/Option.java
+++ src/main/java/org/b3log/solo/model/Option.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * modify notice
+ * add qiniu image style parameter setting qiniuStyle.
  */
 package org.b3log.solo.model;
 
